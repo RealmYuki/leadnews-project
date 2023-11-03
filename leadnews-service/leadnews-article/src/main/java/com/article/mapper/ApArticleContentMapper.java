7209b99fdb70d5057178397efe377f3ca36b7a76
RealmYuki <847443749@qq.com>
2023-11-03 15:49:52 +0800

    文章内容展示

---
+++ leadnews-service/leadnews-article/src/main/java/com/article/mapper/ApArticleContentMapper.java
@@ -0,0 +1,17 @@
+package com.article.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.model.article.pojo.ApArticleContent;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * ClassName: ApArticleContentMapper
+ * Description:
+ * {@code @Author} 苏羽晨
+ * {@code @Create} 2023/11/3 15:46
+ * {@code @Version}  1.0
+ */
+
+@Mapper
+public interface ApArticleContentMapper extends BaseMapper<ApArticleContent> {
+}
